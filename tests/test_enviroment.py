0367ed20ac410228ee72cf14f3027622fc1cd2b3
LockedInTheSkage <LockedInTheSkage@users.noreply.github.com>
2024-09-04 09:35:02 +0200

    fix: Fix typo in test_enviorment

--- tests/test_enviroment.py
+++ tests/test_enviroment.py
@@ -1,7 +1,7 @@
 from src.enviroment import Enviroment
 
-def test_create_environment():
-    env = Environment()
+def test_create_enviroment():
+    env = Enviroment()
     assert env is not None
 
 
