eb540f70716bc64996b23bfd952d5ceea4de83fb
tobiasfremming <tobias.fremming@gmail.com>
2024-09-17 18:29:44 +0200

    feat: Enhance agent parts with new classes and methods for Creature, Limb, and Joint
    Co-authored-by: Nathaniavm <Nathaniavm@users.noreply.github.com>
    Co-authored-by: Parleenb <Parleenb@users.noreply.github.com>

--- src/agent_parts/creature.py
+++ src/agent_parts/creature.py
@@ -5,15 +5,19 @@ from agent import Agent
 from limb import Limb
 from joint import Joint
 
-class Creature():
+class Creature(RenderObject):
+    _limbs: list
+    _joints: list  
+    
+    
     def __init__(self, env):
         self.env = env
     
     def render(self):
         pass
 
-    def creature_factory(env: Enviroment, limblist: list, jointlist: list) -> Creature:
-        """
-        Factory function for creating a creature object.
-        """
-        return Creature(env)
+def creature_factory(env: Enviroment, limblist: list, jointlist: list):
+    """
+    Factory function for creating a creature object.
+    """
+    return Creature(env)
