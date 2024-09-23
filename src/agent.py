76d18fddb5aa87fa82e2637783479cd484d7e50f
tobiasfremming <tobias.fremming@gmail.com>
2024-09-23 11:06:48 +0200

    Refactor agent module and add abstract methods
    This commit refactors the agent module by importing necessary dependencies and reorganizing the code. It also adds abstract methods for the Agent class, including `act`, `save`, `load`, and `get_enviroment_state`. These methods define the behavior of the agent and allow for saving and loading agent data. Additionally, a new method `get_genome` is added to retrieve the agent's genome.

--- src/agent.py
+++ src/agent.py
@@ -1,28 +1,62 @@
-from typing import Protocol
-from enviroment import Enviroment
-from renderObject import RenderObject
-from enum import Enum
+from abc import ABC, abstractmethod
 import pybox2d
+from enum import Enum
+import tensorflow as tf
+
+from src.enviroment import Enviroment
+from src.renderObject import RenderObject
+from src.genome import Genome
+from src.agent_parts.creature import Creature
+
 
 class LimbType(Enum):  
+    """
+    Summary: Enum for the different types of limbs.
+    """
     FOOT = 1
     LEG = 2
     LIMB = 3
 
 
-class Agent(Protocol):
+class Agent(ABC):
+    """
+    
+    """
+    genome: Genome
+    creature: Creature
+    
+    def __init__(self, genome: Genome, creature: Creature) -> None:
+        self.genome = genome
+        self.creature = creature
+
+    @abstractmethod
     def act(self, env) -> int:
         pass
 
-    def save(self, path) -> None:
+    def save(self, path: str) -> None:
+        """
+        Saves the agent and it's creature to a file.
+        """
         pass
 
-    def load(self, path) -> None:
+    def load(self, path: str) -> None:
+        """
+        Loads the agent and it's creature from a file."""
         pass
 
-    def get_genome(self) :
-        pass
+    def get_genome(self) -> Genome:
+        """
+        Returns the genome of the agent.
+        """
+        return self.genome
+    
+    @abstractmethod
+    def get_enviroment_state(self, env) -> tf.Tensor:
+        """
+        Creates the input tensor based on the agent's enviroment and body.
 
+        """
+        pass
 
 
 class Limb():
