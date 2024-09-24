ce0b95b1608a175a09ad918ad18913d964ba81d2
nathanim <nathanim@stud.ntnu.no>
2024-09-24 18:34:49 +0200

    docs: add documentation for each file in agent_parts folder

--- src/agent_parts/creature.py
+++ src/agent_parts/creature.py
@@ -5,13 +5,42 @@ from agent_parts.limb import Limb
 from agent_parts.joint import Joint
 # from joint import Joint
 
-class Creature():
+class Creature:
+    """
+    A class representing a creature composed of limbs and joints in a given environment.
+
+    Attributes:
+    ----------
+    env : Environment
+        The environment in which the creature exists.
+    limblist : list[Limb]
+        A list of Limb objects representing the creature's limbs.
+    jointlist : list[Joint]
+        A list of Joint objects representing the creature's joints.
+    position : list[float, float]
+        The position of the creature represented as a list containing two floats [x, y].
+    """
+    
     env: Environment
     limblist: list[Limb]
     jointlist: list[Joint]
     position: list[float, float]
 
-    def __init__(self, env, limblist: list[Limb], jointlist: list[Joint]):
+    def __init__(self, env: Environment, limblist: list[Limb], jointlist: list[Joint]):
+        """
+        Initializes the Creature object.
+
+        Parameters:
+        ----------
+        env : Environment
+            The environment where the creature is situated.
+        limblist : list[Limb]
+            A list of Limb objects which form the creature.
+        jointlist : list[Joint]
+            A list of Joint objects which connect the limbs of the creature.
+
+        The creature's initial position is set to [200, 200].
+        """
         self.env = env
         self.limblist = limblist
         self.jointlist = jointlist
@@ -27,6 +56,17 @@ class Creature():
             self.jointlist[i].rotate(actions[i])
     
     def render(self, window):
+        """
+        Renders the creature on a given window.
+
+        Parameters:
+        ----------
+        window : any
+            The graphical window where the creature will be rendered. 
+            This can be any object capable of rendering the limbs and joints (e.g., a game screen or canvas).
+
+        The function iterates through all the creature's limbs and joints and calls their respective render method.
+        """
         for limb in self.limblist:
             print(limb)
             limb.render(window)
@@ -34,13 +74,38 @@ class Creature():
             joint.render(window)
     
     def updatePosition(self, x: float, y: float):
+        """
+        Updates the creature's position and propagates the change to all its limbs.
+
+        Parameters:
+        ----------
+        x : float
+            The new x-coordinate of the creature.
+        y : float
+            The new y-coordinate of the creature.
+
+        This method sets the new position of the creature and then updates the position of all limbs accordingly.
+        """
         self.position = [x, y]
         for limb in self.limblist:
             limb.updatePosition(x, y)
 
-def creature_factory(env: Environment, limblist: list[Limb], jointlist: list[Joint]) ->Creature:
+def creature_factory(env: Environment, limblist: list[Limb], jointlist: list[Joint]) -> Creature:
     """
-    Factory function for creating a creature object.
-    """
+    Factory function for creating a Creature object.
 
+    Parameters:
+    ----------
+    env : Environment
+        The environment in which the creature will be created.
+    limblist : list[Limb]
+        A list of Limb objects to form the creature.
+    jointlist : list[Joint]
+        A list of Joint objects to connect the limbs of the creature.
+
+    Returns:
+    -------
+    Creature:
+        A new instance of the Creature class.
+    """
     return Creature(env, limblist, jointlist)
--- src/agent_parts/limb.py
+++ src/agent_parts/limb.py
@@ -5,58 +5,159 @@ from enum import Enum
 import numpy
 import pygame
 
-
 class LimbType(Enum):
+    """
+    Enum representing the different types of limbs a creature can have.
+    
+    Attributes:
+    ----------
+    LIMB : int
+        Represents a standard limb (value 1).
+    HEAD : int
+        Represents the head of the creature (value 2).
+    FOOT : int
+        Represents the foot of the creature (value 3).
+    """
     LIMB = 1
     HEAD = 2     
     FOOT = 3
 
+
 class Limb(RenderObject):
+    """
+    A class representing a limb of a creature, which is a subclass of RenderObject.
+
+    Attributes:
+    ----------
+    rect : Rectangle
+        A Rectangle object representing the shape and position of the limb.
+    damage_scale : float
+        The scale of damage this limb can take, determined by its type.
+    orientation : float
+        The orientation or angle of the limb in the environment.
+    limbType : LimbType
+        An instance of LimbType indicating the specific type of limb (LIMB, HEAD, or FOOT).
+    """
+
     rect: Rectangle
     damage_scale: float 
     orientation: float
     
-    def rotate(self, angle: float):
-        self.orientation += angle
-    
-    def inherit_orientation(self, parent_orientation: float, limb_oreientation: float):
-        self.orientation= parent_orientation  + limb_oreientation
-        
     def __init__(self, rect: Rectangle, damage_scale: float, limbType: LimbType):
+        """
+        Initializes a Limb object.
+
+        Parameters:
+        ----------
+        rect : Rectangle
+            The rectangle representing the shape and size of the limb.
+        damage_scale : float
+            A value representing how much damage the limb can take.
+        limbType : LimbType
+            The type of the limb (LIMB, HEAD, or FOOT).
+        """
         self.rect = rect
         self.damage_scale = damage_scale
         self.limbType = limbType
 
-    def updatePosition(self, x: float, y: float):
-        self.rect.updatePosition(x, y)     
+    def rotate(self, angle: float):
+        """
+        Rotates the limb by a given angle.
 
-    def render(self,window):
+        Parameters:
+        ----------
+        angle : float
+            The angle by which to rotate the limb, added to the current orientation.
+        """
+        self.orientation += angle
+    
+    def inherit_orientation(self, parent_orientation: float, limb_orientation: float):
+        """
+        Inherits the orientation from the parent and adds the limb's own orientation.
+
+        Parameters:
+        ----------
+        parent_orientation : float
+            The orientation of the parent limb or object.
+        limb_orientation : float
+            The local orientation of the limb, relative to the parent.
+        """
+        self.orientation = parent_orientation + limb_orientation
+    
+    def updatePosition(self, x: float, y: float):
+        """
+        Updates the position of the limb by updating the position of its rectangle.
+
+        Parameters:
+        ----------
+        x : float
+            The new x-coordinate of the limb.
+        y : float
+            The new y-coordinate of the limb.
+        """
+        self.rect.updatePosition(x, y)
+
+    def render(self, window):
+        """
+        Renders the limb on the provided window.
+
+        Parameters:
+        ----------
+        window : any
+            The graphical window or surface where the limb will be rendered.
+        """
         self.rect.render(window)
     
-    def getAngle(self):
+    def getAngle(self) -> float:
+        """
+        Retrieves the current orientation angle of the limb.
+
+        Returns:
+        -------
+        float:
+            The current orientation angle of the limb.
+        """
         return self.orientation
 
     def setAngle(self, angle: float):
+        """
+        Sets the orientation of the limb to a specific angle.
+
+        Parameters:
+        ----------
+        angle : float
+            The angle to set as the limb's orientation.
+        """
         self.orientation = angle
-    
-    
 
 
 def limb_factory(rect: Rectangle, limbType: LimbType, orientation: float) -> Limb:
     """
-    Factory function for creating a limb object.
+    Factory function for creating a Limb object.
+
+    Parameters:
+    ----------
+    rect : Rectangle
+        A Rectangle object representing the limb's shape and position.
+    limbType : LimbType
+        The type of limb to be created (LIMB, HEAD, or FOOT).
+    orientation : float
+        The initial orientation angle of the limb.
+
+    Returns:
+    -------
+    Limb:
+        A new instance of the Limb class with the appropriate damage scale based on the limb type.
     """
-    print()
     damage_scale: float = 0
     match limbType:
-        case 1: damage_scale = 0.1
-        case 2: damage_scale = 1
-        case 3: damage_scale = 0
+        case 1:
+            damage_scale = 0.1  # Low damage scale for a standard limb
+        case 2:
+            damage_scale = 1.0  # High damage scale for the head
+        case 3:
+            damage_scale = 0.0  # No damage for the foot
         case _:
             return "Invalid LimbType"
     
-    return Limb(rect, damage_scale, orientation)
-
-
-
-
+    return Limb(rect, damage_scale, limbType)
