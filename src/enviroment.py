1995223bd2b8a95661b5b5637448f9880367e49d
Sindre Fossdal <sindfoss@gmail.com>
2024-09-24 18:55:24 +0200

    feat: add Vision class and abstract ground class
    Co-authored-by: AlMinaDO <AlMinaDO@users.noreply.github.com>

--- src/enviroment.py
+++ src/enviroment.py
@@ -1,6 +1,9 @@
 import pygame as pg
 import random
 import math
+import time 
+       
+from abc import ABC, abstractmethod
 
 from renderObject import RenderObject
 # from src.graphics_facade import GraphicsFacade
@@ -21,16 +24,19 @@ FLOOR_HEIGHT = 100
 screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # Constants for terrain generation
-AMPLITUDE = 50  # Height of hills
-FREQUENCY = 0.02  # Frequency of hills
-SEGMENT_WIDTH = 100  # Width of each terrain segment
+AMPLITUDE_MIN = 30  # Minimum height of hills
+AMPLITUDE_MAX = 40 # Max height of hills
+FREQUENCY_MIN = 0.01  # Minimum frequency of hills
+FREQUENCY_MAX = 0.05 # Max frequency of hills
+SEGMENT_WIDTH = 700  # Width of each terrain segment
 
+    
 
 
 class Environment(RenderObject):
     def __init__(self,  screen):
         self.screen = screen
-        self.ground = Ground(screen, AMPLITUDE, FREQUENCY, SEGMENT_WIDTH)
+        self.ground = Ground(screen, SEGMENT_WIDTH)
         self.scroll_offset = 0
         
     def run(self):
@@ -61,20 +67,51 @@ class Environment(RenderObject):
     def update(self):
         self.ground.generate_new_floor_segment(self.scroll_offset)
         self.ground.remove_old_floor_segment(self.scroll_offset)
-        self.scroll_offset +=5
+        self.scroll_offset +=1
         self.ground.render(self.scroll_offset)
 
 
+class Vision:
+    
+
+    def __init__(self):
+        pass
         
 
-       
+
+class AbstractGround(ABC):
+    @abstractmethod
+    def generate_floor_segment(self, start_x: float) -> list:
+        pass
+
+    @abstractmethod
+    def generate_new_floor_segment(self, scroll_offset: int):
+        pass
+
+    @abstractmethod
+    def remove_old_floor_segment(self, scroll_offset: float):
+        pass
 
     
-class Ground(RenderObject):
-    def __init__(self, screen, amplitude, frequency, segment_width):
+
+
+
+class PerlinGround(RenderObject, AbstractGround):
+
+    defeault_amplitude = 30
+
+
+
+
+
+
+
+
+    
+class Ground(RenderObject, AbstractGround):
+    def __init__(self, screen, segment_width):
         self.screen = screen
-        self.amplitude = amplitude
-        self.frequency = frequency
+       
         self.segment_width = segment_width
         self.terrain_segments = []
 
@@ -90,9 +127,12 @@ class Ground(RenderObject):
         returns:
             list: A list of points representing the floor segment 
         """
+        amplitude = random.randint(AMPLITUDE_MIN, AMPLITUDE_MAX)
+        frequency = random.uniform(FREQUENCY_MIN, FREQUENCY_MAX)
+
         floor= []
-        for x in range(start_x, start_x + self.segment_width + 1, 10):
-            y = int(SCREEN_HEIGHT - FLOOR_HEIGHT + self.amplitude * math.sin(self.frequency * x))
+        for x in range(start_x, start_x + self.segment_width + 1, 1):
+            y = int(SCREEN_HEIGHT - FLOOR_HEIGHT + AMPLITUDE_MAX * math.sin(frequency * x))
             floor.append((x, y))
         return floor
     
@@ -128,6 +168,7 @@ class Ground(RenderObject):
             shifted_points.append((shifted_points[0][0], SCREEN_HEIGHT))
             pg.draw.polygon(screen, (34, 139, 34), shifted_points)  # Green hills
 
+    
 
 
 
