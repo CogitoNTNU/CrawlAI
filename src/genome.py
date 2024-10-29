27537f2c3e3661cb60b2e1d4d7cc855f96e39ed2
tobiasfremming <tobias.fremming@gmail.com>
2024-10-29 18:10:01 +0100

    style: lint

--- src/genome.py
+++ src/genome.py
@@ -7,11 +7,12 @@ class Inovation:
     __instance = None
 
     _global_innovation_counter = 0
-    _innovation_history = {}  # This will track (in_node, out_node) -> innovation_number
+    # This will track (in_node, out_node) -> innovation_number
+    _innovation_history = {} 
 
     @staticmethod
     def get_instance():
-        if Inovation.__instance == None:
+        if Inovation.__instance is None:
             Inovation()
         return Inovation.__instance
 
@@ -21,15 +22,16 @@ class Inovation:
         else:
             Inovation.__instance = self
 
-    def _get_innovation_number(self,in_node,out_node):
+    def _get_innovation_number(self, in_node, out_node):
         
         key = (in_node, out_node)
         if key in self._innovation_history:
             return self._innovation_history[key]
         else:
             self._global_innovation_counter += 1
-            self._innovation_history[(in_node,out_node)] = self._global_innovation_counter
-            return self._innovation_history[(in_node,out_node)]
+            self._innovation_history[(in_node, out_node)] = \
+                self._global_innovation_counter
+            return self._innovation_history[(in_node, out_node)]
           
             
 @dataclass
@@ -41,6 +43,7 @@ class Node:
 
     def __hash__(self) -> int:
         return self.id
+    
 
 @dataclass
 class Connection:
@@ -48,11 +51,18 @@ class Connection:
     in_node: int  
     out_node: int
     weight: float
-    enabled: bool = True   #If there is an edge between the nodes
+    enabled: bool = True   # If there is an edge between the nodes
     innovation_number: int 
+
 
 class Genome:
     """Genome class representing a neural network in NEAT."""
+    id: int 
+    fitness: float
+    nodes: List[Node]
+    connections: List[Connection]
+    species: int
+    adjusted_fitness: float
     
     def __init__(self, genome_id: int, num_inputs: int, num_outputs: int):
         """Create an initial genome with specified input and output nodes."""
@@ -63,7 +73,6 @@ class Genome:
         self.species: int = 0
         self.adjusted_fitness: float = 0.0
 
-        
         # Create input nodes
         for i in range(num_inputs):
             self.nodes.add(Node(id=i, node_type='input'))
@@ -76,17 +85,22 @@ class Genome:
         for input_node in range(num_inputs):
             for output_node in range(num_outputs):
                 self.connections.append(Connection(
-                    in_node = input_node,
-                    out_node = num_inputs + output_node,
-                    weight = random.uniform(-1.0, 1.0),  # Random weight between -1 and 1
-                    innovation_number = get_innovation_number(input_node, num_inputs + output_node)
+                    in_node=input_node,
+                    out_node=num_inputs + output_node,
+                    # Random weight between -1 and 1
+                    weight=random.uniform(-1.0, 1.0),  
+                    innovation_number=get_innovation_number(
+                        input_node, 
+                        num_inputs + output_node)
                 )) 
 
     def __str__(self):
-        return f"Genome ID: {self.id}, Fitness: {self.fitness}, Species: {self.species}, Adjusted Fitness: {self.adjusted_fitness}"
+        return f"Genome ID: {self.id}, Fitness: {self.fitness},"
+        f"Species: {self.species}, Adjusted Fitness: {self.adjusted_fitness}"
 
     def __repr__(self):
-        return f"Genome ID: {self.id}, Fitness: {self.fitness}, Species: {self.species}, Adjusted Fitness: {self.adjusted_fitness}"
+        return f"Genome ID: {self.id}, Fitness: {self.fitness},"
+        f"Species: {self.species}, Adjusted Fitness: {self.adjusted_fitness}"
 
     def __eq__(self, other):
         return self.id == other.id
@@ -95,7 +109,18 @@ class Genome:
         return self.fitness < other.fitness
 
 
-def get_innovation_number(in_node, out_node):   # Method that gets or sets innovation number
+def get_innovation_number(in_node, out_node): 
+    """
+    Method that gets or sets innovation number for a 
+    connection between two nodes.
+
+    Args:
+        in_node (_type_)
+        out_node (_type_)
+
+    Returns:
+        _type_: global_innovation_counter
+    """
     global global_innovation_counter
     global innovation_history
 
@@ -106,7 +131,12 @@ def get_innovation_number(in_node, out_node):   # Method that gets or sets innov
         innovation_history[(in_node, out_node)] = global_innovation_counter
         return global_innovation_counter
 
-def initialize_population(pop_size: int, num_inputs: int, num_outputs: int) -> List[Genome]:
+
+def initialize_population(
+        pop_size: int,
+        num_inputs: int,
+        num_outputs: int
+        ) -> List[Genome]:
     """Initialize a population of genomes."""
     population = []
     for genome_id in range(pop_size):
@@ -122,7 +152,10 @@ if __name__ == "__main__":
     num_input_nodes = 3  # For example, 3 input nodes
     num_output_nodes = 1  # For example, 1 output node
 
-    population = initialize_population(population_size, num_input_nodes, num_output_nodes)
+    population = initialize_population(
+        population_size,
+        num_input_nodes,
+        num_output_nodes)
     
     # Print the initialized genomes
     for genome in population:
