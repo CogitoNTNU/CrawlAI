db66523d9bef2d751503bb1a7754845c6b41b799
Nilsyyyyyyy <nhlund@ntnu.no>
2024-11-03 15:12:00 +0100

    Main working on genetic algorithm

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def main():
     running = True
     while running:
         for event in pygame.event.get():
-            if event.type == QUIT:
+            if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
